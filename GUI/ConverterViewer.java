755eda632ce6f2cfc040eef4598d804b76725aac
Connie Zhou <omnawmrawr@gmail.com>
2016-03-08 13:43:57 -0700

    Added files via upload

---
+++ GUI/ConverterViewer.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class ConverterViewer
+{  
+   public static void main(String[] args)
+   {  
+      JFrame frame = new ConverterFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+   }
+}
